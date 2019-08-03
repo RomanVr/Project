e01f4f5bc32b9dda4d1b135abc0337d46da64de1
RomanVr <romakf99@yandex.ru>
2019-08-03 19:19:52 +0300

    include react-router-dom and setting routing

--- app/src/components/App/App.js
+++ app/src/components/App/App.js
@@ -1,52 +1,60 @@
-import React, { Component } from 'react';
-import _ from 'lodash';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import FormReg from '../Form/FormReg';
 import FormLogin from '../Form/FormLogin';
 import Nav from '../Nav';
 
-const routes = {
-  root: {
-    path: '',
-    component: FormLogin,
-  },
-  sessionNew: {
-    path: '/session/new',
-    component: FormReg,
-  },
-  session: {
-    path: '/session',
-    component: FormLogin,
-  },
-};
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      route: window.location.hash.substr(1),
-    };
-    console.log('--- route', this.state.route);
-  }
-
-  componentDidMount() {
-    window.addEventListener('hashchange', () => {
-      this.setState({
-        route: window.location.hash.substr(1),
-      });
-      console.log('--- route', this.state.route);
-    });
-  }
-
-  render() {
-    const Child = routes[_.findKey(routes, { path: this.state.route })];
-    return (
-      <div className = "container">
-        <Nav />
-        <div style = {{ marginTop: 80 }}/>
-        <Child.component />
+function App() {
+  return (
+    <div className = "container">
+      <Nav />
+      <div style = {{ marginTop: 80 }}/>
+      <div>
+        <Switch>
+          <Route exact path="/" component = { FormLogin } />
+          <Route path = "/session/new" component = { FormReg } />
+          <Route path = "/session" component = { FormLogin } />
+          <Route path = "/chat" component = { FormLogin } />
+        </Switch>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
+
+// const routes = {
+//   root: {
+//     path: '',
+//     component: FormLogin,
+//   },
+//   sessionNew: {
+//     path: '/session/new',
+//     component: FormReg,
+//   },
+//   session: {
+//     path: '/session',
+//     component: FormLogin,
+//   },
+// };
+
+// class App extends Component {
+// constructor(props) {
+//   super(props);
+//   this.state = {
+//     route: window.location.hash.substr(1),
+//   };
+//   console.log('--- route', this.state.route);
+// }
+
+// componentDidMount() {
+//   window.addEventListener('hashchange', () => {
+//     this.setState({
+//       route: window.location.hash.substr(1),
+//     });
+//     console.log('--- route', this.state.route);
+//   });
+// }
+
+// render() {
+// const Child = routes[_.findKey(routes, { path: this.state.route })];
--- app/src/components/Nav/index.js
+++ app/src/components/Nav/index.js
@@ -1,24 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function () {
   return (
     <div className = "container">
       <nav className = "navbar navbar-expand-lg navbar-info fixed-top bg-dark">
-        <a className = 'navbar-brand' href = '/'>AEON</a>
+        <Link className = 'navbar-brand' to = '/'>AEON</Link>
         <div className="collapse navbar-collapse d-flex justify-content-between" id="navbarSupportedContent">
         <ul className="navbar-nav">
           <li className="nav-item">
-              <a className="nav-link" href="#/chat">Чат</a>
+              <Link className="nav-link" to = "/chat">Чат</Link>
           </li>
         </ul>
           <ul className="navbar-nav">
             <li className="nav-item">
-                <a className="nav-link" href="#/session/new">Регистрация</a>
+                <Link className="nav-link" to = "/session/new">Регистрация</Link>
             </li>
           </ul>
           <ul className="navbar-nav">
             <li className="nav-item">
-                <a className="nav-link" href="#/session">Войти</a>
+                <Link className="nav-link" to="/session">Войти</Link>
             </li>
 
           </ul>
